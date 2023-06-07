33098b3d51ecee77ca7630e3b800d71dc6657a45
Josh Dolitsky <josh@dolit.ski>
2023-06-07 16:56:59 -0500

    Drop dependency on monopod, convert all images to terraform
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

---
+++ images/alpine-base/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+    oci  = { source = "chainguard-dev/oci" }
+  }
+}
+
+variable "target_repository" {
+  description = "The docker repo into which the image and attestations should be published."
+}
+
+provider "apko" {
+  extra_repositories = ["https://dl-cdn.alpinelinux.org/alpine/edge/main"]
+  extra_keyring      = []
+  default_archs      = ["386", "amd64", "arm64", "arm/v6", "arm/v7", "ppc64le", "riscv64", "s390x"]
+}
+
+module "latest" {
+  source            = "../../tflib/publisher"
+  target_repository = var.target_repository
+  config            = file("${path.module}/configs/latest.apko.yaml")
+  extra_packages    = []
+}
+
+module "test-latest" {
+  source = "./tests"
+  digest = module.latest.image_ref
+}
+
+resource "oci_tag" "version-tags" {
+  depends_on = [ module.test-latest ]
+  for_each   = toset(["latest"])
+  digest_ref = module.latest.image_ref
+  tag        = "${each.key}"
+}
---
+++ images/melange/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+    oci  = { source = "chainguard-dev/oci" }
+  }
+}
+
+variable "target_repository" {
+  description = "The docker repo into which the image and attestations should be published."
+}
+
+provider "apko" {
+  extra_repositories = ["https://packages.wolfi.dev/os", "${path.module}/../../packages"]
+  extra_keyring      = ["https://packages.wolfi.dev/os/wolfi-signing.rsa.pub", "${path.module}/../../melange.rsa.pub"]
+  default_archs      = ["arm64", "amd64"]
+}
+
+module "latest" {
+  source            = "../../tflib/publisher"
+  target_repository = var.target_repository
+  config            = file("${path.module}/configs/latest.apko.yaml")
+  extra_packages    = ["wolfi-baselayout"]
+}
+
+module "test-latest" {
+  source = "./tests"
+  digest = module.latest.image_ref
+}
+
+resource "oci_tag" "version-tags" {
+  depends_on = [ module.test-latest ]
+  for_each   = toset(["latest"])
+  digest_ref = module.latest.image_ref
+  tag        = "${each.key}"
+}
